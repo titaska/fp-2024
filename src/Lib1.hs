743a38c4d0a1af4fcf8133a927a5a566a3e5c0f8
Titas <titaskaritonas@gmail.com>
2024-10-06 18:31:09 +0300

    task 1

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,8 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = ["add",
+                "remove",
+                "list",
+                "find",
+                "exit"]
